91d077807e0119c4ca475a561be112af527a717b
Arthur Torres <arthurtorres002@gmail.com>
2018-12-12 15:31:39 -0500

    finished product

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,19 +7,42 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  character_info = response_hash["results"].find do |character_info|
+    character_info["name"].downcase == character_name
+  end
+  film_urls = character_info["films"]
+
+  film_urls.map do |film_url|
+    response_string = RestClient.get(film_url)
+    response_hash = JSON.parse(response_string)
+  end
+end
+
+def get_movie_info_from_api(movie)
+  response_string = RestClient.get('http://www.swapi.co/api/films/')
+  response_hash = JSON.parse(response_string)
+  response_hash["results"].find do |movie_hash|
+    movie_hash["title"].downcase == movie
+  end
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    puts film["title"]
+  end
+end
+
+def print_movie_info(film)
+  film.delete("characters")
+  film.delete("planets")
+  film.delete("starships")
+  film.delete("vehicles")
+  film.delete("species")
+  film.each do |attribute, value|
+    puts "#{attribute}: "
+    puts value
+    puts ""
+  end
 end
 
 def show_character_movies(character)
@@ -27,7 +50,7 @@ def show_character_movies(character)
   print_movies(films)
 end
 
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+def show_movie_info(movie)
+  film = get_movie_info_from_api(movie)
+  print_movie_info(film)
+end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,28 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome!"
+end
+
+def get_search_method
+  puts "Would you like to search by character or by movie?"
+  search_method = gets.chomp.downcase
+  if search_method == "movie"
+    get_movie_from_user
+  elsif  search_method == "character"
+    get_character_from_user
+  else
+    puts "Invalid entry try again"
+    get_search_method
+  end
+end
+
+def get_movie_from_user
+  puts "Please enter a movie name"
+  movie = gets.chomp.downcase
+  show_movie_info(movie)
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character name"
+  character = gets.chomp.downcase
+  show_character_movies(character)
 end
